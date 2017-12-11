cee5ba084ff970861a70210e69823a39cd33bca9
Robert Niederreiter <office@squarewave.at>
2017-12-11 15:58:13 +0100

    WIP

--- webresource/markup.py
+++ webresource/markup.py
@@ -1,7 +1,7 @@
 from webresource.resource import resource_registry as rr
 
 
-def js_resources(development=False):
+def js_markup(development=False):
     """Return JS loading markup.
 
     :param development: Flag whether development mode.
@@ -9,7 +9,7 @@ def js_resources(development=False):
     """
 
 
-def css_resources(development=False):
+def css_markup(development=False):
     """Return CSS loading markup.
 
     :param development: Flag whether development mode.
