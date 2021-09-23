444c9621b47953a736037aed6821db4f9e21ddce
Robert Niederreiter <office@squarewave.at>
2021-09-23 20:09:30 +0200

    prepare 1.0b8

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.0b8 (unreleased)
+1.0b8 (2021-09-23)
 ------------------
 
 - Rename ``hash_`` keyword argument of resources to ``unique``.
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.0b8.dev0'
+version = '1.0b8'
 shortdesc = "A resource registry for web applications."
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
