f61e5874716b6c89278cae1a32330448a012909c
Robert Niederreiter <rnix@squarewave.at>
2022-12-21 09:59:55 +0100

    Update version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021-2022, Cone Contributors'
 author = 'Cone Contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.2'
 
 
 # -- General configuration ---------------------------------------------------
